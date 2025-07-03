0696cf6f80bf32516e65e8a0def19adf2a631c51
Himanshu Raj <rajhimanshu7452@gmail.com>
2025-07-03 14:48:13 +0530

    Updated

--- script.js
+++ script.js
@@ -12,3 +12,97 @@ const maxScoreSpan = document.getElementById("max-score");
 const progressBar = document.getElementById("progress");
 const resultMessage = document.getElementById("result-message");
 const restartButton = document.getElementById("restart-btn");
+
+const quizQuestions = [
+    {
+    question: "What is the capital of France?",
+    answers: [
+      { text: "London", correct: false },
+      { text: "Berlin", correct: false },
+      { text: "Paris", correct: true },
+      { text: "Madrid", correct: false },
+    ],
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    answers: [
+      { text: "Venus", correct: false },
+      { text: "Mars", correct: true },
+      { text: "Jupiter", correct: false },
+      { text: "Saturn", correct: false },
+    ],
+  },
+  {
+    question: "What is the largest ocean on Earth?",
+    answers: [
+      { text: "Atlantic Ocean", correct: false },
+      { text: "Indian Ocean", correct: false },
+      { text: "Arctic Ocean", correct: false },
+      { text: "Pacific Ocean", correct: true },
+    ],
+  },
+  {
+    question: "Which of these is NOT a programming language?",
+    answers: [
+      { text: "Java", correct: false },
+      { text: "Python", correct: false },
+      { text: "Banana", correct: true },
+      { text: "JavaScript", correct: false },
+    ],
+  },
+  {
+    question: "What is the chemical symbol for gold?",
+    answers: [
+      { text: "Go", correct: false },
+      { text: "Gd", correct: false },
+      { text: "Au", correct: true },
+      { text: "Ag", correct: false },
+    ],
+  },
+];
+
+let currentQuestionIndex = 0;
+let score = 0;
+let answersDisabled = false;
+
+totalQuestionsSpan.textContent = quizQuestions.length;
+maxScoreSpan.textContent = quizQuestions.length;
+
+startButton.addEventListener("click", startQuiz);
+restartButton.addEventListener("click", restartQuiz);
+
+function startQuiz() {
+    // reset
+    currentQuestionIndex = 0;
+    score = 0;
+    scoreSpan.textContent = 0;
+
+    startScreen.classList.remove("active");
+    quizScreen.classList.add("active");
+}
+
+function showQuestion() {
+    // reset state
+    answersDisabled = false;
+
+    const currentQuestion = quizQuestions[currentQuestionIndex]
+
+    currentQuestionSpan.textContent = currentQuestionIndex + 1;
+
+    const progressBar = (currentQuestionIndex / quizQuestions.length) * 100;
+    progressBar.style.width = progressPercent + "%";
+
+    answerContainer.innerHTML = "";
+
+    currentQuestion.answers.map(()=>{
+        const button = document.createElement("button");
+        button.textContent = answers.text;
+        button.classList.add("answer-btn");
+
+        button.dataset.correct = answerContainer.correct;
+
+        button.addEventListener("click", selectAnswer);
+
+        answerContainer.appendChild(button);
+    })
+}
